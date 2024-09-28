a22858b454e96f8a369d7a033c38192a8c1a5b40
Frogman2001 <88066509+Frogman2001@users.noreply.github.com>
2024-09-28 13:02:47 -0500

    Update TestOpMode.java

--- TeamCode/src/main/java/edu/edina/opmodes/Test/TestOpMode.java
+++ TeamCode/src/main/java/edu/edina/opmodes/Test/TestOpMode.java
@@ -14,6 +14,7 @@ public class TestOpMode extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
+    private int SelectedItem = 0;
 
     @Override
     public void runOpMode() {
@@ -30,9 +31,37 @@ public class TestOpMode extends LinearOpMode {
             double yDriveInput = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             Boolean leftBumperPushed =  gamepad1.left_bumper;
 
-            if(leftBumperPushed){
 
+            if(leftBumperPushed){
+                if(SelectedItem <= 2){
+                    SelectedItem++;
+
+                }else{
+                    SelectedItem =0;
+                }
             }
+
+            switch(SelectedItem){
+                case 0:
+                    leftFrontDrive.setPower(yDriveInput);
+                    telemetry.addData("SelectedItem","leftFrontDrive");
+                    break;
+                case 1:
+                    rightFrontDrive.setPower(yDriveInput);
+                    telemetry.addData("SelectedItem","rightFrontDrive");
+                    break;
+                case 2:
+                    leftBackDrive.setPower(yDriveInput);
+                    telemetry.addData("SelectedItem","leftBackDrive");
+                    break;
+                case 3:
+                    rightBackDrive.setPower(yDriveInput);
+                    telemetry.addData("SelectedItem","rightBackDrive");
+                    break;
+            }
+
+            telemetry.addData("leftStickInput",yDriveInput);
+            telemetry.update();
         }
     }
 }
