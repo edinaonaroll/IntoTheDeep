679f01f44fafaa029b195f29aba98966c88c10ec
Ddddevco <Devonc.sparkz3d@gmail.com>
2024-09-28 13:37:16 -0500

    tidied up spacing

--- TeamCode/src/main/java/edu/edina/opmodes/Test/TestOpMode.java
+++ TeamCode/src/main/java/edu/edina/opmodes/Test/TestOpMode.java
@@ -3,6 +3,7 @@ package edu.edina.opmodes.Test;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import edu.edina.definitions.BotBits;
@@ -14,6 +15,9 @@ public class TestOpMode extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
+    private double yDriveInput = gamepad1.left_stick_y;
+
+    private Boolean leftBumperPushed = false;
     private int SelectedItem = 0;
 
     @Override
@@ -23,28 +27,40 @@ public class TestOpMode extends LinearOpMode {
         rightFrontDrive = hardwareMap.get(DcMotor.class, BotBits.FrontRightDriveMotor);
         rightBackDrive = hardwareMap.get(DcMotor.class, BotBits.BackRightDriveMotor);
 
+        leftFrontDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
 
         telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
         waitForStart();
 
         while (opModeIsActive()) {
-            double yDriveInput = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            Boolean leftBumperPushed =  gamepad1.left_bumper;
+            yDriveInput = gamepad1.left_stick_y;
+            leftBumperPushed =  gamepad1.left_bumper;
 
-
-            if(leftBumperPushed){
+            if (leftBumperPushed){
                 if(SelectedItem <= 2){
                     SelectedItem++;
-
-                }else{
+                } else {
                     SelectedItem =0;
                 }
+//                wait(1000);
+//                Thread.sleep(1000)throws InterruptedException;
             }
 
-            switch(SelectedItem){
+            switch (SelectedItem) {
                 case 0:
                     leftFrontDrive.setPower(yDriveInput);
                     telemetry.addData("SelectedItem","leftFrontDrive");
+                    telemetry.addData("MotorDirection",leftFrontDrive.getDirection());
                     break;
                 case 1:
                     rightFrontDrive.setPower(yDriveInput);
